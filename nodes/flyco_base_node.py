a1bc7bd03651a1f8840f9989c620650d570e9634
David Tsai <dtsai@berkeley.edu>
2016-11-12 15:05:06 -0800

    Initial Joule setup

---
+++ nodes/flyco_base_node.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+import rospy
+
+if __name__ == "__main__":
+    rospy.init_node("flyco_base_node")
+    rospy.loginfo("flyco_base_node starting")
+    rospy.loginfo("flyco_base_node finished")
