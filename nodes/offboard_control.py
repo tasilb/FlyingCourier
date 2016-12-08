a9b0f8deb185c697224705cc88e6094e6c28332d
Lucy Chen <lucy96_chen@berkeley.edu>
2016-12-07 16:40:00 -0800

    Code fixed, tested via Gazebo simulation

--- nodes/offboard_control.py
+++ nodes/offboard_control.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-# Code adapted from MAVROS offboard example at
+# Adapted from code located at
 # dex.px4.io/ros-mavros-offboard.html
+# and https://github.com/ucdart/UAV_Commander/blob/master/TCS/default_offboard.py
 import rospy, tf, sys
 import std_msgs.msg
 from geometry_msgs.msg import PoseStamped
@@ -10,7 +11,10 @@ from mavros_msgs.srv import CommandBool, SetMode
 current_state = State()
 
 def callback(msg):
-    current_state = msg
+    current_state.armed = msg.armed
+    current_state.connected = msg.connected
+    current_state.mode = msg.mode
+    current_state.guided = msg.guided
 
 if __name__ == "__main__":
     rospy.init_node('offb_node')
@@ -19,7 +23,7 @@ if __name__ == "__main__":
     local_pos_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=1)
     
     rate = rospy.Rate(20)
-    while (not rospy.is_shutdown) and (current_state.connected):
+    while not current_state.connected:
         rate.sleep()
         
     pose = PoseStamped()
@@ -28,17 +32,8 @@ if __name__ == "__main__":
     pose.pose.position.z = 2
     
     for i in range(100):
-        if rospy.is_shutdown():
-            print ("Unable to continue streaming setpoints")
-            sys.exit()
         local_pos_pub.publish(pose)
         rate.sleep()
-    
-    offb_set_mode = SetMode()
-    offb_set_mode.custom_mode = 'OFFBOARD'
-    
-    arm_cmd = CommandBool()
-    arm_cmd.value = True
     
     last_request = rospy.Time.now()
 
@@ -46,15 +41,15 @@ if __name__ == "__main__":
         if (current_state.mode != 'OFFBOARD') and (rospy.Time.now() - last_request > rospy.Duration(5.0)):
             rospy.wait_for_service('mavros/set_mode')
             set_mode_client = rospy.ServiceProxy('mavros/set_mode', SetMode)
-            mode_resp = set_mode_client(offb_set_mode)
+            mode_resp = set_mode_client(0, 'OFFBOARD')
             if mode_resp.success:
                 rospy.loginfo("OFFBOARD ENABLED")
             last_request = rospy.Time.now()
         else:
-            if (current_state.mode != 'OFFBOARD') and (rospy.Time.now() - last_request > rospy.Duration(5.0)):
+            if (not current_state.armed) and (rospy.Time.now() - last_request > rospy.Duration(5.0)):
                 rospy.wait_for_service('mavros/cmd/arming')
                 arming_client = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
-                arm_resp = arming_client(arm_cmd)
+                arm_resp = arming_client(True)
                 if arm_resp.success:
                     rospy.loginfo("VEHICLE ARMED")
                 last_request = rospy.Time.now()
