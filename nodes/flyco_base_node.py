b0477f4c6682bd70cc81b136bacf88f9719fad78
David Tsai <tcdavid372@gmail.com>
2016-12-10 00:18:29 -0800

    Add log messages to base node

--- nodes/flyco_base_node.py
+++ nodes/flyco_base_node.py
@@ -15,6 +15,7 @@ class FlycoBaseNode:
         self._local_position_sub = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self._on_pose)
         self._cmd_sub = rospy.Subscriber("/flyco/cmd", FlycoCmd, self._on_cmd)
         self._status_pub = rospy.Publisher("/flyco/main_status", FlycoStatus, queue_size=1)
+        rospy.log("[FlycoBase] FlycoBase initialized!")
 
     def run(self):
         while not rospy.is_shutdown():
@@ -36,14 +37,18 @@ class FlycoBaseNode:
             rospy.log("[FlycoBase] Currently in fault state, command rejected.")
             break
         elif cmdType == FlycoCmd.CMD_FAILSAFE:
+            rospy.log("[FlycoBase] Entering fault mode.")
             self._status.status = FlycoStatus.STATUS_FAULT
         elif cmdType == FlycoCmd.CMD_LAND:
+            rospy.log("[FlycoBase] Entering landing mode.")
             self._status.status = FlycoStatus.STATUS_LANDING
         elif cmdType == FlycoCmd.CMD_SETPOINT_POS:
+            rospy.log("[FlycoBase] Entering entering setpoint navigation mode.")
             self._status.status = FlycoStatus.STATUS_SETPOINT_NAV
             self._status.setpoint_type = FlycoStatus.SETPOINT_POS
             self._status.setpoint_pose = msg.setpoint_pose
         elif cmdType == FlycoCmd.CMD_ACTUATE:
+            rospy.log("[FlycoBase] Entering actuation mode.")
             self._status.status = FlycoStatus.STATUS_ACTUATION
 
 if __name__ == "__main__":
